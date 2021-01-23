f904c554361f5ae09b052fb1a2d69e45e230cae2
Carlos Ramirez <ramirezcoronelcarlos@gmail.com>
2021-01-23 16:15:34 -0400

    reporte de servicios

--- app/Controllers/ReporteController.php
+++ app/Controllers/ReporteController.php
@@ -4,7 +4,9 @@ namespace App\Controllers;
 
 use App\Models\Compra;
 use App\Models\Venta;
+use App\Models\Servicio;
 use App\Models\Usuario;
+use App\Models\Empleado;
 use App\Traits\Utility;
 use Exception;
 use PDO;
@@ -16,6 +18,8 @@ class ReporteController extends Controller {
     private $compra;
     private $venta;
     private $usuario;
+    private $empleado;
+    private $servicio;
 	
 	use Utility;
 
@@ -24,6 +28,8 @@ class ReporteController extends Controller {
         $this->compra = new Compra;
         $this->venta = new Venta;
         $this->usuario = new Usuario;
+        $this->empleado = new Empleado;
+        $this->servicio = new Servicio;
     }
     
     public function index(){
@@ -35,8 +41,10 @@ class ReporteController extends Controller {
                 usuarios WHERE estatus='Activo'");
         $query->execute();
         $usuarios = $query->fetchAll(PDO::FETCH_OBJ);
+        $tecnicos = $this->empleado->getTecnicos();
         return View::getView('Reporte.index',[
-            'usuarios' => $usuarios
+            'usuarios' => $usuarios,
+            'tecnicos' => $tecnicos
         ]);
     }
     public function reporteVenta()
@@ -124,6 +132,109 @@ class ReporteController extends Controller {
         }
         
 
+        $html = ob_get_clean();
+
+        $this->crearPDF($html);
+       
+    }
+    
+    public function reporteServicio()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if( $method != 'POST'){
+            http_response_code(404);
+            return false;
+        }  
+        $empleado = $_POST['tecnico']; 
+        $desde = $_POST['desde']; 
+        $hasta = $_POST['hasta']; 
+        $desde.= " 00:00:00";
+        $hasta.= " 23:59:59";
+        $tecnico = NULL;
+        if($empleado == 0){
+            $query = $this->servicio->connect()->prepare("SELECT p.codigo, date_format(p.fecha, '%d-%m-%Y %r') as fecha,
+                c.nombre as cliente, CONCAT(e.nombre, ' ', e.apellido) as empleado, ROUND(d.precio,2) as total, s.nombre as nombre_servicio
+                FROM servicios_prestados p 
+                INNER JOIN clientes c 
+                ON p.cliente_id = c.id 
+                INNER JOIN empleados e 
+                ON p.empleado_id = e.id 
+                INNER JOIN detalle_servicio d 
+                ON d.servicio_prestado_id=p.id
+                INNER JOIN servicios s
+                ON  d.servicio_id = s.id
+                WHERE p.estatus = 'ACTIVO' AND p.fecha BETWEEN
+                :desde AND :hasta GROUP BY d.servicio_prestado_id ORDER BY p.fecha DESC
+            ");
+            $tecnicos = true;
+            // $query2 = $this->venta->connect()->prepare("SELECT v.codigo, v.fecha, vp.metodo, count(vp.metodo) as cantidad, SUM(vp.monto) as 
+            //     total FROM ventas v LEFT JOIN venta_pago vp ON v.id = vp.venta_id 
+            //     WHERE v.estatus = 'ACTIVO' AND v.fecha BETWEEN
+            //     :desde AND :hasta GROUP BY vp.metodo ");
+        }
+        else{
+
+            $query = $this->servicio->connect()->prepare("SELECT p.codigo, date_format(p.fecha, '%d-%m-%Y %r') as fecha,
+                c.nombre as cliente, CONCAT(e.nombre, ' ', e.apellido) as empleado, ROUND(d.precio,2) as total, s.nombre as nombre_servicio
+                FROM servicios_prestados p INNER JOIN clientes c ON p.cliente_id = c.id 
+                INNER JOIN empleados e 
+                ON p.empleado_id = e.id 
+                INNER JOIN detalle_servicio d 
+                ON d.servicio_prestado_id=p.id 
+                INNER JOIN servicios s
+                ON  d.servicio_id = s.id
+                WHERE p.estatus = 'ACTIVO' AND p.empleado_id = :empleado AND p.fecha BETWEEN
+                :desde AND :hasta GROUP BY d.servicio_prestado_id ORDER BY p.fecha DESC
+            ");
+
+            $query->bindParam(':empleado',$empleado);
+            $tecnicos = false;
+            // $query2 = $this->venta->connect()->prepare("SELECT v.codigo, v.fecha, vp.metodo, count(vp.metodo) as cantidad, SUM(vp.monto) as 
+            //     total FROM ventas v LEFT JOIN venta_pago vp ON v.id = vp.venta_id 
+            //     WHERE v.estatus = 'ACTIVO' AND v.usuario_id = :usuario AND v.fecha BETWEEN
+            //     :desde AND :hasta GROUP BY vp.metodo ");
+            // $query2->bindParam(':usuario',$usuario);
+            $tecnico = $this->empleado->getOne("empleados", $empleado);
+        }
+        $query->bindParam(':desde',$desde);
+        $query->bindParam(':hasta',$hasta);
+        $query->execute();
+        $servicios = $query->fetchAll(PDO::FETCH_OBJ);
+        // $query2->bindParam(':desde',$desde);
+        // $query2->bindParam(':hasta',$hasta);
+        // $query2->execute();
+        // $pagos = $query2->fetchAll(PDO::FETCH_OBJ);
+        // $dolar = $this->venta->getAll('dolar');
+        ob_start();
+        if ($tecnicos) {
+            View::getViewPDF('FormatosPDF.reporteServicio',[
+                'servicios' => $servicios,
+                // 'pagos' => $pagos,
+                'desde' => $desde,
+                'hasta' => $hasta,
+                'dolar' => 1,
+                // 'dolar' => $dolar[0]->precio,
+                'tecnicos' => $tecnicos,
+                'cantidad' => 0,
+                'total' => 0
+            ]);
+        }
+        else{
+            View::getViewPDF('FormatosPDF.reporteServicio',[
+                'servicios' => $servicios,
+                // 'pagos' => $pagos,
+                'desde' => $desde,
+                'hasta' => $hasta,
+                'dolar' => 1,
+                // 'dolar' => $dolar[0]->precio,
+                'tecnicos' => $tecnicos,
+                'cantidad' => 0,
+                'total' => 0,
+                'tecnico' =>$tecnico->nombre." ".$tecnico->apellido
+            ]);
+        }
+        
         $html = ob_get_clean();
 
         $this->crearPDF($html);
---
+++ app/Views/contents/FormatosPDF/reporteServicio.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Reporte de Servicios</title>
+
+    <style type="text/css">
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        .container{
+            padding: 0px 0px;
+            /* color:lightslategrey; */
+            color:black;
+        }
+        .padding{
+            padding: 2px;
+        }
+        .margin{
+            margin: 2px 0px;
+        }
+        .text-center {
+            text-align: center;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+        .centrado{
+            margin: auto;
+        }
+        .clear{
+            clear: both;
+        }
+        .separador{
+            display:block; 
+            width:100%; 
+            height:1px; 
+            margin: 1px 0px;
+            background: gray; 
+        }
+        .separadorDos{
+            display:block; 
+            width:100%; 
+            height:1px; 
+            margin: 2px 0px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container centrado" style="width: 98%;">
+
+        <div class="centrado" style="width=98%;">
+            <h1 class="text-center margin">REPORTE DE SERVICIOS</h1>
+            <hr>
+            <div>
+            
+                <p>
+                <?php if(!$tecnicos){?>
+                    <strong>Tecnico: </strong> <?=$tecnico?><br>
+                <?php }?>
+                 <strong>Desde: </strong> <?=$desde?><br>
+                 <strong>Hasta: </strong> <?=$hasta?>
+                </p>
+                
+            </div>
+            <!-- Servicios -->
+            <?php if(isset($servicios) && count($servicios)>0){?>
+            <div clas="separadorDos"></div>
+            <h3 class="text-center margin"><u>SERVICIOS</u></h3><br>
+        
+        
+        <?php if($tecnicos){?>
+            <div>
+                <div style="width:11%; display:inline;" class="text-center">
+                    <strong>CÓDIGO</strong>
+                </div>
+                <div style="width:11.5%; display:inline;" class="text-center">
+                    <strong>FECHA</strong>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center">
+                    <strong>CLIENTE</strong>
+                </div>
+                <div style="width:15.5%; display:inline;" class="text-center">
+                    <strong>TECNICO</strong>
+                </div>
+                <div style="width:15.5%; display:inline;" class="text-center">
+                    <strong>SERVICIO</strong>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center">
+                    <strong>TOTAL $</strong>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center">
+                    <strong>TOTAL BSS</strong>
+                </div>
+            </div>
+            
+            <hr><br>
+            <?php                    
+                $total = 0;
+                foreach($servicios AS $servicio):
+                    $cantidad++;
+                    $total += $servicio->total;
+            ?>
+
+            <div>
+                <div style="width:11%; display:inline;" class="text-center" >
+                    <span><?= $servicio->codigo; ?></span>
+                </div>
+                <div style="width:11.5%; display:inline;" class="text-center" >
+                    <span><?= $servicio->fecha; ?></span>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center" >
+                    <span><?= $servicio->cliente; ?></span>
+                </div>
+                <div style="width:15.5%; display:inline;" class="text-center" >
+                    <span><?= $servicio->empleado; ?></span>
+                </div>
+                <div style="width:15.5%; display:inline;" class="text-center" >
+                    <span><?= $servicio->nombre_servicio; ?></span>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center" >
+                    <span><?= $servicio->total ?></span>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center" >
+                    <span><?= $servicio->total * $dolar; ?></span>
+                </div>
+            </div>
+                
+            <br>
+            <div class="separador"></div>
+                <?php
+                    endforeach;
+                ?>
+            <div>
+                <div style="width:22.5%; display:inline;" class="text-center" >
+                    <b>Cantidad de Servicios</b>
+                </div>
+                <div style="width:23.5%; display:inline;" class="text-center" >
+                    <span><?= $cantidad; ?></span>
+                </div>
+                <!-- <div style="width:15.5%; display:inline;" class="text-center" >
+                    <span></span>
+                </div> -->
+                <div style="width:18.5%; display:inline;" class="text-center" >
+                    <b>Total</b>
+                </div>
+                <div style="width:17%; display:inline;" class="text-center" >
+                    <span><?= $total ?></span>
+                </div>
+                <div style="width:14%; display:inline;" class="text-center" >
+                    <span><?= $total * $dolar; ?></span>
+                </div>
+            </div>
+                
+            <br>
+            <div class="separador"></div>
+            <?php }else{?>
+                <div>
+                    <div style="width:16.6%; display:inline;" class="text-center">
+                        <strong>CÓDIGO</strong>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center">
+                        <strong>FECHA</strong>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center">
+                        <strong>CLIENTE</strong>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center">
+                        <strong>SERVICIO</strong>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center">
+                        <strong>TOTAL $</strong>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center">
+                        <strong>TOTAL BSS</strong>
+                    </div>
+                </div>
+
+                <hr><br>
+                <?php                    
+                    $total = 0;
+                    foreach($servicios AS $servicio):
+                        $cantidad++;
+                        $total += $servicio->total;
+                ?>
+
+                <div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $servicio->codigo; ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $servicio->fecha; ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $servicio->cliente; ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $servicio->nombre_servicio; ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $servicio->total ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $servicio->total * $dolar; ?></span>
+                    </div>
+                </div>
+                
+                <br>
+                <div class="separador"></div>
+                <?php
+                    endforeach;
+                ?>
+                <div>
+                    <div style="width:33.33%; display:inline;" class="text-center" >
+                        <b>Cantidad de Servicios</b>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $cantidad ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <b>Total</b>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $total ?></span>
+                    </div>
+                    <div style="width:16.6%; display:inline;" class="text-center" >
+                        <span><?= $total * $dolar; ?></span>
+                    </div>
+                </div>
+                
+                <br>
+                <div class="separador"></div>
+            <?php }?>
+            <?php } ?>
+            <br> 
+                
+        </div>  
+        <!-- Fin de Servicios -->
+       
+        <br>
+        <div class="separadorDos"></div>
+        <div class="separadorDos"></div>
+        <!-- Métodos de Pago -->
+        <?php if(isset($pagos) AND count($pagos)>0){?>
+        <div class="centrado" style="width: 95%;">
+                <h3 class="text-center margin"><u>MÉTODOS DE PAGO</u></h3><br>
+                <div>
+                    <!-- <p> <b>Total de Servicios: </b> <?=$cantidad;?></p> -->
+                   
+                    <div style="width:24%; display:inline;" class="text-center">
+                        <strong>MÉTODO</strong>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center">
+                        <strong>CANTIDAD</strong>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center">
+                        <strong>TOTAL $</strong>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center">
+                        <strong>TOTAL BSS</strong>
+                    </div>
+                    
+                </div>
+
+                <hr><br>
+                <?php
+                    $cantidadPagos = 0;
+                    $totalPagos = 0;
+                    foreach ($pagos as $pago): 
+                        if(isset($pago->metodo)){
+                            $cantidadPagos += $pago->cantidad;
+                            $totalPagos += $pago->total;
+                ?>
+                <div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $pago->metodo; ?></span>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $pago->cantidad; ?></span>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $pago->total; ?></span>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $pago->total*$dolar ?></span>
+                    </div>
+                </div>
+                
+                <br>
+                <div class="separador"></div>
+                <?php }endforeach;?>
+                <div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <b>TOTAL</b>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $cantidadPagos ?></span>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $totalPagos ?></span>
+                    </div>
+                    <div style="width:24%; display:inline;" class="text-center" >
+                        <span><?= $totalPagos*$dolar ?></span>
+                    </div>
+                </div>
+                <br>
+                <div class="separador"></div>
+        </div>
+        <?php }?>
+        <!--Fin Métodos de Pago  -->
+           
+            
+    </div>
+</body>
+</html>
