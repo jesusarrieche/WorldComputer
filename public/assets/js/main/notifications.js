eb40be49bc47684b69c09f8ce1071be61ef85ae5
Carlos Ramirez <ramirezcoronelcarlos@gmail.com>
2021-02-03 07:54:57 -0400

    fix: caja notificaciones

--- public/assets/js/main/notifications.js
+++ public/assets/js/main/notifications.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
         var color = $(this).text();
         if (down) {
 
+            $('#box').css('display', 'none');
             $('#box').css('height', '0px');
             $('#box').css('opacity', '0');
             down = false;
         } else {
 
+            $('#box').css('display', 'block');
             $('#box').css('height', 'auto');
             $('#box').css('opacity', '1');
             down = true;
